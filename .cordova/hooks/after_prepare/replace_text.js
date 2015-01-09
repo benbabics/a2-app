375abc179aa1d5004af705e5b843856b51e89bd9
Erin Doyle <Erin.Doyle@wexinc.com>
2015-01-09 13:21:35 +0000

    Cleaned up JSHint warnings

--- .cordova/hooks/after_prepare/replace_text.js
+++ .cordova/hooks/after_prepare/replace_text.js
@@ -3,16 +3,18 @@
 // this plugin replaces arbitrary text in arbitrary files
 //
 
-var fs = require('fs');
-var path = require('path');
+/* jshint -W106 */ // suppress the jshint warnings about non camel case functions
+
+var fs = require("fs");
+var path = require("path");
 
 var rootdir = process.argv[2];
 
 function replace_string_in_file(filename, to_replace, replace_with) {
-    var data = fs.readFileSync(filename, 'utf8');
+    var data = fs.readFileSync(filename, "utf8");
 
     var result = data.replace(to_replace, replace_with);
-    fs.writeFileSync(filename, result, 'utf8');
+    fs.writeFileSync(filename, result, "utf8");
 }
 
 function update_file(rootdir, filename, target, platform, configobj) {
@@ -20,7 +22,12 @@ function update_file(rootdir, filename, target, platform, configobj) {
     if (fs.existsSync(fullfilename)) {
         replace_string_in_file(fullfilename, "@@@STRING_REPLACE_APP_URL@@@", configobj[target].app_url);
 
+        console.log("App URL set to: " + configobj[target].app_url);
+
         replace_string_in_file(fullfilename, "@@@STRING_REPLACE_APP_STORE_URL@@@", configobj[platform].app_store_url);
+
+        console.log("App Store URL set to: " + configobj[target].app_store_url);
+
     } else {
         console.log("missing: "+fullfilename);
     }
@@ -33,12 +40,12 @@ if (process.env.TARGET) {
 
 if (rootdir) {
     var ourconfigfile = path.join(rootdir, "project.json");
-    var configobj = JSON.parse(fs.readFileSync(ourconfigfile, 'utf8'));
+    var configobj = JSON.parse(fs.readFileSync(ourconfigfile, "utf8"));
 
 
     update_file(rootdir, "platforms/android/assets/www/js/globals.js", target, "android", configobj);
 
     update_file(rootdir, "platforms/ios/www/js/globals.js", target, "ios", configobj);
-    
+
     replace_string_in_file(path.join(rootdir, "platforms/android/AndroidManifest.xml"), "userPortrait", "portrait");
 }
