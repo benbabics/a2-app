f6f356a4045fc6902242a232c3952e9d068c11fd
Jeff Guinness <Jeff.Guinness@wexinc.com>
2015-08-06 16:25:36 +0000

    MOBACCTMGT-119 - MOBACCTMGT-64: Username field is allowing for more than 30 characters

---
+++ test/unit/shared/widgets/wexMaxLength.directive.spec.js
@@ -0,0 +1,65 @@
+(function () {
+    "use strict";
+
+    describe("A WEX Max Length directive", function () {
+
+        var form, $scope, element;
+
+        beforeEach(function () {
+            module("app.shared.widgets");
+
+            // INJECT! This part is critical
+            // $rootScope - injected to create a new $scope instance.
+            // $compile - injected to allow us test snippets produced by the directive
+            inject(function($rootScope, $compile) {
+                $scope = $rootScope.$new();
+
+                //null out the input value to start
+                $scope.model = { textInput: null };
+
+                //Compile the angular markup to get a form that uses the directive
+                element = angular.element("<form name='form'><input name='textInput' wex-max-length='10' ng-model='model.textInput'></form>");
+                $compile(element)($scope);
+                form = $scope.form;
+            });
+
+        });
+
+        describe("modifies an input field that", function () {
+
+            it("should NOT modify the value when the input length is less than the max length", function () {
+                var value = TestUtils.getRandomStringThatIsAlphaNumeric(9);
+
+                form.textInput.$setViewValue(value);
+                $scope.$digest();
+
+                expect(form.textInput.$viewValue).toEqual(value);
+                expect($scope.model.textInput).toEqual(value);
+            });
+
+            it("should NOT modify the value when the input length equals the max length", function () {
+                var value = TestUtils.getRandomStringThatIsAlphaNumeric(10);
+
+                form.textInput.$setViewValue(value);
+                $scope.$digest();
+
+                expect(form.textInput.$viewValue).toEqual(value);
+                expect($scope.model.textInput).toEqual(value);
+            });
+
+            it("should modify the value when the input length is greater than the max length", function () {
+                var expectedValue = TestUtils.getRandomStringThatIsAlphaNumeric(10),
+                    enteredValue = expectedValue + TestUtils.getRandomStringThatIsAlphaNumeric(1);
+
+                form.textInput.$setViewValue(enteredValue);
+                $scope.$digest();
+
+                expect(form.textInput.$viewValue).toEqual(expectedValue);
+                expect($scope.model.textInput).toEqual(expectedValue);
+            });
+
+        });
+
+    });
+
+}());
