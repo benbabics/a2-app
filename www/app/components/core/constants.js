e783095f018321ca7cf8837aeac28bc3891ba2f3
Jeff Guinness <Jeff.Guinness@wexinc.com>
2015-08-14 21:06:52 +0000

    MOBACCTMGT-258 - Change the menu and payment confirmation to go to the new route

--- www/app/components/core/constants.js
+++ www/app/components/core/constants.js
@@ -10,81 +10,81 @@
 
     appGlobals.USER_LOGIN = {
         "CONFIG": {
-            "title": "WEX Fleet Manager",
-            "userName": {
-                "label": "User Name",
+            "title"       : "WEX Fleet Manager",
+            "userName"    : {
+                "label"    : "User Name",
                 "maxLength": 30
             },
-            "password": {
-                "label": "Password",
+            "password"    : {
+                "label"    : "Password",
                 "maxLength": 30
             },
             "submitButton": "Log In",
             "serverErrors": {
-                "AUTHORIZATION_FAILED": "Your account is not able to be managed via the mobile application at this time.",
-                "DEFAULT": "Invalid login information. Please check your username and password or go online to set up or recover your username and password.",
-                "PASSWORD_EXPIRED": "Invalid login information. Go online to set up or recover your username and password.",
-                "TOKEN_EXPIRED": "Your session has expired. Please login again.",
-                "USER_LOCKED": "You have exceeded the number of allowable login attempts. You will need to access your online account to retrieve your username and password.",
-                "USER_MUST_ACCEPT_TERMS": "Invalid login information. Go online to set up or recover your username and password.",
+                "AUTHORIZATION_FAILED"              : "Your account is not able to be managed via the mobile application at this time.",
+                "DEFAULT"                           : "Invalid login information. Please check your username and password or go online to set up or recover your username and password.",
+                "PASSWORD_EXPIRED"                  : "Invalid login information. Go online to set up or recover your username and password.",
+                "TOKEN_EXPIRED"                     : "Your session has expired. Please login again.",
+                "USER_LOCKED"                       : "You have exceeded the number of allowable login attempts. You will need to access your online account to retrieve your username and password.",
+                "USER_MUST_ACCEPT_TERMS"            : "Invalid login information. Go online to set up or recover your username and password.",
                 "USER_MUST_SETUP_SECURITY_QUESTIONS": "Invalid login information. Go online to set up or recover your username and password.",
-                "USER_NOT_ACTIVE": "Invalid login information. Go online to set up or recover your username and password."
+                "USER_NOT_ACTIVE"                   : "Invalid login information. Go online to set up or recover your username and password."
             }
         }
     };
 
     appGlobals.LANDING = {
         "CONFIG": {
-            "title": "WEX Fleet Manager",
+            "title"          : "WEX Fleet Manager",
             "availableCredit": "Available",
-            "paymentDueDate": "Payment Due Date",
-            "currentBalance": "Current Balance",
-            "minimumPayment": "Minimum Payment",
-            "makePayment": "Make Payment",
-            "viewActivity": "View Activity",
-            "cards": "Cards",
-            "drivers": "Drivers"
+            "paymentDueDate" : "Payment Due Date",
+            "currentBalance" : "Current Balance",
+            "minimumPayment" : "Minimum Payment",
+            "makePayment"    : "Make Payment",
+            "viewActivity"   : "View Activity",
+            "cards"          : "Cards",
+            "drivers"        : "Drivers"
         }
     };
 
     appGlobals.PAYMENT_ADD = {
-        "CONFIG": {
-            "title": "Make Payment",
-            "invoiceNumber": "Invoice Number",
+        "CONFIG"  : {
+            "title"         : "Make Payment",
+            "invoiceNumber" : "Invoice Number",
             "paymentDueDate": "Payment Due Date",
             "currentBalance": "Current Balance",
             "minimumPayment": "Minimum Payment",
-            "enterAmount": "Enter Amount",
-            "amount": "Amount",
-            "bankAccount": "From Account",
-            "scheduledDate": "Date",
-            "submitButton": "Next"
+            "enterAmount"   : "Enter Amount",
+            "amount"        : "Amount",
+            "bankAccount"   : "From Account",
+            "scheduledDate" : "Date",
+            "submitButton"  : "Next"
         },
         "WARNINGS": {
-            "BANK_ACCOUNTS_NOT_SETUP": "You must set up your financial institutions as your payment options online prior to scheduling a payment.",
-            "DIRECT_DEBIT_SETUP": "Online payment is not currently available for this account. The account has set up an alternative method of payment, such as direct debit.",
-            "NO_BALANCE_DUE": "Current Balance needs to be greater than $0.00.",
+            "BANK_ACCOUNTS_NOT_SETUP"  : "You must set up your financial institutions as your payment options online prior to scheduling a payment.",
+            "DIRECT_DEBIT_SETUP"       : "Online payment is not currently available for this account. The account has set up an alternative method of payment, such as direct debit.",
+            "NO_BALANCE_DUE"           : "Current Balance needs to be greater than $0.00.",
             "PAYMENT_ALREADY_SCHEDULED": "A payment has been scheduled already."
         },
-        "INPUTS": {
-            "AMOUNT": {
+        "INPUTS"  : {
+            "AMOUNT"      : {
                 "CONFIG": {
                     "title": "Amount"
                 },
                 "ERRORS": {
-                    "zeroPayment": "The amount entered cannot be \"0.00\".",
+                    "zeroPayment"    : "The amount entered cannot be \"0.00\".",
                     "paymentTooLarge": "The amount entered cannot be greater than the Current Balance"
                 }
             },
-            "DATE": {
+            "DATE"        : {
                 "CONFIG": {
-                    "title": "Date",
+                    "title"        : "Date",
                     "maxFutureDays": 180
                 }
             },
             "BANK_ACCOUNT": {
                 "CONFIG": {
-                    "title": "Account",
+                    "title"            : "Account",
                     "instructionalText": "Please Note: Managing bank accounts can only be completed on the web from the desktop application version."
                 }
             }
@@ -92,35 +92,35 @@
     };
 
     appGlobals.PAYMENT_SUMMARY = {
-        "CONFIG": {
-            "title": "Payment Summary",
-            "afternoonWarning": "Payments received after 3:30 pm Eastern Time may not be posted to your account until the next business day.",
+        "CONFIG"  : {
+            "title"                : "Payment Summary",
+            "afternoonWarning"     : "Payments received after 3:30 pm Eastern Time may not be posted to your account until the next business day.",
             "weekendHolidayWarning": "Scheduled payments will be processed on the date selected. Payments scheduled for weekends/holidays will be posted the next business day.",
-            "amount": "Amount",
-            "bankAccount": "From Account",
-            "scheduledDate": "Date",
-            "cancelButton": "Cancel",
-            "submitButton": "Schedule Payment"
+            "amount"               : "Amount",
+            "bankAccount"          : "From Account",
+            "scheduledDate"        : "Date",
+            "cancelButton"         : "Cancel",
+            "submitButton"         : "Schedule Payment"
         },
         "WARNINGS": {
             "PAYMENT_AMOUNT_LESS_THAN_MINIMUM": "This amount is less than the minimum payment.",
-            "PAYMENT_DATE_PAST_DUE_DATE": "The date selected for payment is past the due date."
+            "PAYMENT_DATE_PAST_DUE_DATE"      : "The date selected for payment is past the due date."
         }
     };
 
     appGlobals.PAYMENT_CONFIRMATION = {
         "CONFIG": {
-            "title": "Payment Confirmation",
+            "title"           : "Payment Confirmation",
             "confirmationText": "Your payment has been scheduled.",
-            "amount": "Amount",
-            "bankAccount": "From Account",
-            "scheduledDate": "Date",
-            "activityButton": "Payment Activity"
+            "amount"          : "Amount",
+            "bankAccount"     : "From Account",
+            "scheduledDate"   : "Date",
+            "activityButton"  : "Payment Activity"
         }
     };
 
     appGlobals.PAYMENT_LIST = {
-        "CONFIG": {
+        "CONFIG"        : {
             "title"                     : "Payment Activity",
             "scheduledPaymentsHeading"  : "Scheduled Payments",
             "noScheduledPaymentsMessage": "There are currently no payments scheduled.",
@@ -142,14 +142,14 @@
     appGlobals.MENU = {
         "CONFIG": {
             "options": {
-                "home": "Home",
-                "makePayment": "Make Payment",
+                "home"        : "Home",
+                "makePayment" : "Make Payment",
                 "viewActivity": "View Activity",
-                "cards": "Cards",
-                "drivers": "Drivers",
-                "paymentHistory": "Payment Activity",
-                "contactUs": "Contact Us",
-                "logOut": "Log Out"
+                "cards"       : "Cards",
+                "drivers"     : "Drivers",
+                "payments"    : "Payment Activity",
+                "contactUs"   : "Contact Us",
+                "logOut"      : "Log Out"
             }
         }
     };
@@ -157,7 +157,7 @@
     appGlobals.BUTTONS = {
         "CONFIG": {
             "cancel": "Cancel",
-            "done": "Done"
+            "done"  : "Done"
         }
     };
 
