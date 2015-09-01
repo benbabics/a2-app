405440806c7c54b41b941f064983ea64950d91aa
Mychal Thompson <Mychal.Thompson@wexinc.com>
2015-09-01 14:47:05 +0000

    MOBACCTMGT-379 - Added a transaction module to components and moved transaction templates to it +review CR-MOBACCTMGT

---
+++ test/unit/components/transaction/transaction.route.spec.js
@@ -0,0 +1,48 @@
+(function () {
+    "use strict";
+
+    describe("A Transaction Module Route Config", function () {
+
+        var $rootScope,
+            $state;
+
+        beforeEach(function () {
+
+            module("app.shared");
+            module("app.components.transaction");
+            module("app.html");
+
+            inject(function (_$rootScope_, _$state_) {
+                $rootScope = _$rootScope_;
+                $state = _$state_;
+            });
+        });
+
+        describe("has a transaction state that", function () {
+            var state,
+                stateName = "transaction";
+
+            beforeEach(function() {
+                state = $state.get(stateName);
+            });
+
+            it("should be defined", function() {
+                expect(state).toBeDefined();
+                expect(state).not.toBeNull();
+            });
+
+            it("should be abstract", function() {
+                expect(state.abstract).toBeTruthy();
+            });
+
+            it("should have the expected URL", function() {
+                expect(state.url).toEqual("/transaction");
+            });
+
+            it("should define a view on the root container", function() {
+                expect(state.views).toBeDefined();
+                expect(state.views["@"]).toBeDefined();
+            });
+        });
+    });
+})();
--- www/app/components/components.module.js
+++ www/app/components/components.module.js
@@ -3,14 +3,15 @@
 
     angular.module("app.components", [
         /* app component modules */
-        "app.components.core",
         "app.components.account",
         "app.components.bank",
-        "app.components.user",
+        "app.components.core",
         "app.components.landing",
         "app.components.invoice",
-        "app.components.payment",
         "app.components.menu",
-        "app.components.navBar"
+        "app.components.navBar",
+        "app.components.payment",
+        "app.components.transaction",
+        "app.components.user"
     ]);
 })();
