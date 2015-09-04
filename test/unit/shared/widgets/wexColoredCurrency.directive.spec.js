457d8a09c91617913f3dfb31c2cbef44218399c2
Jeff Guinness <Jeff.Guinness@wexinc.com>
2015-09-04 20:11:18 +0000

    MOBACCTMGT-393 - Create directive for styling numbers green if positive and red if negative and use on Available balance field

---
+++ test/unit/shared/widgets/wexColoredCurrency.directive.spec.js
@@ -0,0 +1,156 @@
+(function () {
+    "use strict";
+
+    describe("A WEX Colored Currency directive", function () {
+
+        var $scope,
+            element,
+            model;
+
+        beforeEach(function () {
+            module("app.shared");
+            module("app.html");
+
+            // INJECT! This part is critical
+            // $rootScope - injected to create a new $scope instance.
+            // $compile - injected to allow us test snippets produced by the directive
+            inject(function($rootScope, $compile) {
+                $scope = $rootScope.$new();
+
+                // null out the amount value to start
+                $scope.model = { amount: null };
+
+                // Compile the angular markup to get an element that uses the directive
+                element = angular.element("<span wex-colored-currency ng-model='model.amount'></span>");
+                $compile(element)($scope);
+                model = $scope.model;
+            });
+
+        });
+
+        describe("modifies an element that", function () {
+
+            describe("when the amount is greater than zero", function () {
+
+                beforeEach(function () {
+                    model.amount = TestUtils.getRandomNumber(0.01, 9999);
+                    $scope.$digest();
+                });
+
+                it("should add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-positive")).toBeTruthy();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-negative")).toBeFalsy();
+                });
+
+            });
+
+            describe("when the amount is zero", function () {
+
+                beforeEach(function () {
+                    model.amount = 0;
+                    $scope.$digest();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-positive")).toBeFalsy();
+                });
+
+                it("should add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-negative")).toBeTruthy();
+                });
+
+            });
+
+            describe("when the amount is less than zero", function () {
+
+                beforeEach(function () {
+                    model.amount = 0 - TestUtils.getRandomNumber(0.01, 9999);
+                    $scope.$digest();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-positive")).toBeFalsy();
+                });
+
+                it("should add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-negative")).toBeTruthy();
+                });
+
+            });
+
+            describe("when the amount is a object", function () {
+
+                beforeEach(function () {
+                    model.amount = {};
+                    $scope.$digest();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-positive")).toBeFalsy();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-negative")).toBeFalsy();
+                });
+
+            });
+
+            describe("when the amount is a string", function () {
+
+                beforeEach(function () {
+                    model.amount = TestUtils.getRandomStringThatIsAlphaNumeric(1);
+                    $scope.$digest();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-positive")).toBeFalsy();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-negative")).toBeFalsy();
+                });
+
+            });
+
+            describe("when the amount is null", function () {
+
+                beforeEach(function () {
+                    model.amount = null;
+                    $scope.$digest();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-positive")).toBeFalsy();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-negative")).toBeFalsy();
+                });
+
+            });
+
+            describe("when the amount is undefined", function () {
+
+                beforeEach(function () {
+                    delete model.amount;
+                    $scope.$digest();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-positive")).toBeFalsy();
+                });
+
+                it("should NOT add the 'balance-positive' class to the content element", function () {
+                    expect(element.hasClass("balance-negative")).toBeFalsy();
+                });
+
+            });
+
+        });
+
+    });
+
+}());
---
+++ www/app/shared/widgets/wexColoredCurrency.directive.js
@@ -0,0 +1,47 @@
+(function () {
+    "use strict";
+
+    /* jshint -W003 */
+    /* jshint -W026 */ // These allow us to show the definition of the Directive above the scroll
+
+    function wexColoredCurrency(CommonService) {
+        var _ = CommonService._,
+            directive = {
+                restrict: "A",
+                link: link,
+                scope: {
+                    model: "=ngModel"
+                }
+            },
+            directiveElem,
+            vm;
+
+
+        function setClass() {
+            if (_.isNumber(vm.model)) {
+                if (vm.model > 0) {
+                    directiveElem.addClass("balance-positive");
+                }
+                else {
+                    directiveElem.addClass("balance-negative");
+                }
+            }
+        }
+
+        function link(scope, elem) {
+            vm = scope;
+            directiveElem = elem;
+
+            //functions
+            scope.setClass = _.bind(setClass, scope, _);
+
+            //watchers
+            scope.$watch(scope.model, scope.setClass);
+        }
+
+        return directive;
+    }
+
+    angular.module("app.shared.widgets")
+        .directive("wexColoredCurrency", wexColoredCurrency);
+}());
