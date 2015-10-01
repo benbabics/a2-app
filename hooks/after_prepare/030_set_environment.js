2b2e14de31e291155aa331069ecaac28014048e2
Jeff Guinness <Jeff.Guinness@wexinc.com>
2015-10-01 00:40:17 +0000

    Prepare for iOS 9 - Copy hook and associated changes from MCA per code review feedback

--- hooks/after_prepare/030_set_environment.js
+++ hooks/after_prepare/030_set_environment.js
@@ -10,6 +10,8 @@
 
 var fs = require("fs");
 var path = require("path");
+var _ = require("lodash");
+var plist = require("plist");
 
 var rootdir = process.argv[2];
 
@@ -24,6 +26,24 @@ function replaceStringInFile(filename, to_replace, replace_with) {
     catch(e) {
         console.log(e);
     }
+}
+
+function updateIosPlist(targetFile, configItems) {
+    var infoPlist = plist.parse(fs.readFileSync(targetFile, "utf-8")),
+        tempInfoPlist;
+
+    _.each(configItems, function (item) {
+        var key = item.parent;
+        var plistXml = "<plist><dict><key>" + key + "</key>";
+        plistXml += item.data + "</dict></plist>";
+
+        var configPlistObj = plist.parse(plistXml);
+        infoPlist[key] = configPlistObj[key];
+    });
+
+    tempInfoPlist = plist.build(infoPlist);
+    tempInfoPlist = tempInfoPlist.replace(/<string>[\s\r\n]*<\/string>/g,"<string></string>");
+    fs.writeFileSync(targetFile, tempInfoPlist, "utf-8");
 }
 
 function updateConstants(rootdir, fileName, target, configObj) {
@@ -61,6 +81,72 @@ function updateIndex(rootdir, fileName, target, configObj) {
     }
 }
 
+function whiteListDomainsOnIos(rootdir, target, configObj) {
+    var KEY_APP_TRANSPORT_SECURITY = "NSAppTransportSecurity",
+        KEY_EXCEPTION_DOMAINS = "NSExceptionDomains",
+        KEY_ALLOW_ARBITRARY_LOADS = "NSAllowsArbitraryLoads",
+        DICT_WHITELIST_SETTINGS = "" +
+            "<dict>" +
+            "   <key>NSIncludesSubdomains</key>" +
+            "   <true/>" +
+            "   <key>NSExceptionAllowsInsecureHTTPLoads</key>" +
+            "   <true/>" +
+            "   <key>NSExceptionRequiresForwardSecrecy</key>" +
+            "   <false/>" +
+            "   <key>NSExceptionMinimumTLSVersion</key>" +
+            "   <string>TLSv1.2</string>" +
+            "   <key>NSThirdPartyExceptionAllowsInsecureHTTPLoads</key>" +
+            "   <true/>" +
+            "   <key>NSThirdPartyExceptionRequiresForwardSecrecy</key>" +
+            "   <false/>" +
+            "   <key>NSThirdPartyExceptionMinimumTLSVersion</key>" +
+            "   <string>TLSv1.2</string>" +
+            "</dict>";
+
+    var fileName = "platforms/ios/AcctMaint/AcctMaint-Info.plist",// TODO: get the project name from config.xml
+        fullFileName = path.join(rootdir, fileName);
+
+    if (fs.existsSync(fullFileName)) {
+
+        var whiteListDomains = configObj[target].whitelist_domains,
+            whiteListConfig = {},
+            plistXml = "";
+
+        if (whiteListDomains && whiteListDomains.length > 0) {
+
+            // disable App Transport Security
+            if (whiteListDomains[0] === "*") {
+                plistXml = "<dict><key>" + KEY_ALLOW_ARBITRARY_LOADS + "</key><true/></dict>";
+            }
+            // add the App Transport Security settings with the white listed domains added as exceptions
+            else {
+                plistXml = "<dict><key>" + KEY_EXCEPTION_DOMAINS + "</key><dict>";
+
+                _.each(whiteListDomains, function (domain) {
+                    console.log("In " + fullFileName + " - setting WhiteList for domain: " + domain);
+
+                    plistXml += "<key>" + domain + "</key>";
+                    plistXml += DICT_WHITELIST_SETTINGS;
+                });
+
+                plistXml += "</dict></dict>";
+            }
+
+        }
+        // enable App Transport Security on all domains
+        else {
+            plistXml = "<dict><key>" + KEY_ALLOW_ARBITRARY_LOADS + "</key><false/></dict>";
+        }
+
+        whiteListConfig.parent = KEY_APP_TRANSPORT_SECURITY;
+        whiteListConfig.data = plistXml;
+        updateIosPlist(fullFileName, [whiteListConfig]);
+
+    } else {
+        console.log("ERROR missing " + fullFileName + " file.");
+    }
+}
+
 if (rootdir) {
     var target = "stage"; //default to stage
     if (process.env.TARGET) {
@@ -84,6 +170,10 @@ if (rootdir) {
         updateIndex(rootdir, "platforms/ios/www/index.html", target, configObj);
         updateIndex(rootdir, "platforms/browser/www/index.html", target, configObj);
 
+        // Add whitelisted domains for each platform
+        // Android not needed at this time
+        whiteListDomainsOnIos(rootdir, target, configObj);
+        // browser not needed at this time
 
     } catch(e) {
         console.log(e);
