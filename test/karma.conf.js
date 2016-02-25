523096af587ccb6eb19af6ea794b59fc38f8a49a
Mychal Thompson <Mychal.Thompson@wexinc.com>
2016-02-25 20:36:58 +0000

    Removed loki-indexed-adapter.js from karma.conf +review CR-MOBACCTMGT-1001

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -38,7 +38,6 @@ module.exports = function(config) {
             "lib/ngstorage/ngStorage.js",
             "lib/lokijs/src/lokijs.js",
             "lib/lokijs/src/loki-angular.js",
-            "lib/lokijs/src/loki-indexed-adapter.js",
 
             // Test libraries
             "../test/lib/**/*.js",
