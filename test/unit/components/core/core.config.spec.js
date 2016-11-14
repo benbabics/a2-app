9472d04f26bb92ffe946e9a340457311d8672075
Ben Babics <Ben.Babics@wexinc.com>
2016-11-14 11:22:45 -0500

    MOBACCTMGT-1003 tests are green

--- test/unit/components/core/core.config.spec.js
+++ test/unit/components/core/core.config.spec.js
@@ -3,12 +3,15 @@
 
     describe("A Core Module config", function () {
 
-        var $compileProvider,
+        var $rootScope,
+            $state,
+            $compileProvider,
             $ionicConfigProvider,
             $urlRouterProvider,
             IdleProvider,
             ionicDatePickerProvider,
             appGlobals,
+            AuthenticationManager,
             sharedGlobals;
 
         beforeAll(function () {
@@ -17,7 +20,6 @@
         });
 
         beforeEach(function () {
-
             module("app.shared", function (_$compileProvider_, _$ionicConfigProvider_, _$urlRouterProvider_, _IdleProvider_, _ionicDatePickerProvider_) {
                 $ionicConfigProvider = _$ionicConfigProvider_;
                 $urlRouterProvider = _$urlRouterProvider_;
@@ -36,10 +38,20 @@
 
             this.includeDependencies();
 
-            inject(function(_appGlobals_, _sharedGlobals_) {
+            // mock deps:
+            AuthenticationManager = jasmine.createSpyObj("AuthenticationManager", ["logOut", "userLoggedIn"]);
+
+            inject(function(_$rootScope_, _$state_, _appGlobals_, _sharedGlobals_) {
+                $rootScope = _$rootScope_;
+                $state = _$state_;
                 appGlobals = _appGlobals_;
                 sharedGlobals = _sharedGlobals_;
+
+                spyOn( $rootScope, "$on" ).and.callThrough();
+                spyOn( $urlRouterProvider, "rule" ).and.callThrough();
             });
+
+            appGlobals.LOGIN_STATE = "user.auth.login";
         });
 
         it("should set the default route to the expected value", function () {
@@ -68,6 +80,47 @@
 
         it("should set the expected properties for the date picker", function () {
             expect(ionicDatePickerProvider.configDatePicker).toHaveBeenCalledWith(sharedGlobals.DATE_PICKER);
+        });
+
+        describe("should set a $urlRouterProvider rule handler that", function () {
+            var loginRoute      = "user.auth.login",
+                landingRoute    = "landing",
+                paymentAddRoute = "payment.add";
+
+            describe("when the user is logged in", function () {
+                beforeEach(function () {
+                    AuthenticationManager.userLoggedIn.and.returnValue(true);
+                });
+
+                describe("when the user is navigating to the login page", function () {
+                    beforeEach(function () {
+                        $state.go(loginRoute);
+                        $rootScope.$digest();
+                    });
+
+                    it("should continue to the page", function () {
+                        expect($state.current.name).toEqual(loginRoute);
+                    });
+                });
+
+            });
+
+            describe("when the user is NOT logged in", function () {
+                beforeEach(function () {
+                    AuthenticationManager.userLoggedIn.and.returnValue(false);
+                });
+
+                describe("when the user is navigating to the login page", function () {
+                    beforeEach(function () {
+                        $state.go(loginRoute);
+                        $rootScope.$digest();
+                    });
+
+                    it("should continue to the page", function () {
+                        expect($state.current.name).toEqual(loginRoute);
+                    });
+                });
+            });
         });
 
     });
