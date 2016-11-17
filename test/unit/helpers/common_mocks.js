5f69979e1a11009a87704d328eb6ade148542104
mthompson <mychal.thompson@wexinc.com>
2016-11-16 22:25:58 -0500

    Updated angular-mocks and fixed memory leak in common_mocks.

--- test/unit/helpers/common_mocks.js
+++ test/unit/helpers/common_mocks.js
@@ -1,57 +1,65 @@
-beforeEach(function () {
+(function () {
     "use strict";
 
-    var self = this;
+    var self;
 
-    self.includeDependencies = function () {
-        if (self.includeSharedDependencies) {
-            module("app.shared", ["$provide", _.partial(TestUtils.provideCommonSharedMockDependencies, _, function (mocks) {
-                //add all common mocks to the currently executing test suite
-                _.forEach(mocks, function (mock, name) {
-                    _.set(self, name, mock);
+    beforeEach(function () {
+        self = this;
+
+        self.includeDependencies = function () {
+            if (self.includeSharedDependencies) {
+                module("app.shared", ["$provide", _.partial(TestUtils.provideCommonSharedMockDependencies, _, function (mocks) {
+                    //add all common mocks to the currently executing test suite
+                    _.forEach(mocks, function (mock, name) {
+                        _.set(self, name, mock);
+                    });
+                }, self.commonSharedMockExclusions)]);
+            }
+
+            if (self.includeAppDependencies) {
+                self.includeHtml = true;
+
+                module("app.components", ["$provide", _.partial(TestUtils.provideCommonAppMockDependencies, _, function (mocks) {
+                    //add all common mocks to the currently executing test suite
+                    _.forEach(mocks, function (mock, name) {
+                        _.set(self, name, mock);
+                    });
+                }, self.commonAppMockExclusions)]);
+            }
+
+            if (self.includeHtml) {
+                module("app.html");
+            }
+            else {
+                // stub the routing and template loading
+                module(function ($urlRouterProvider) {
+                    $urlRouterProvider.deferIntercept();
                 });
-            }, self.commonSharedMockExclusions)]);
-        }
 
-        if (self.includeAppDependencies) {
-            self.includeHtml = true;
-
-            module("app.components", ["$provide", _.partial(TestUtils.provideCommonAppMockDependencies, _, function (mocks) {
-                //add all common mocks to the currently executing test suite
-                _.forEach(mocks, function (mock, name) {
-                    _.set(self, name, mock);
+                module(function ($provide) {
+                    $provide.value("$ionicTemplateCache", _.noop);
                 });
-            }, self.commonAppMockExclusions)]);
+            }
+        };
+
+        if (self.includeSharedDependencies === undefined) {
+            self.includeSharedDependencies = true;
         }
 
-        if (self.includeHtml) {
-            module("app.html");
+        if (self.includeAppDependencies === undefined) {
+            self.includeAppDependencies = true;
         }
-        else {
-            // stub the routing and template loading
-             module(function ($urlRouterProvider) {
-             $urlRouterProvider.deferIntercept();
-             });
 
-             module(function ($provide) {
-             $provide.value("$ionicTemplateCache", _.noop);
-             });
+        if (self.includeHtml === undefined) {
+            self.includeHtml = false;
         }
-    };
 
-    if (self.includeSharedDependencies === undefined) {
-        self.includeSharedDependencies = true;
-    }
+        if (!self.deferIncludes) {
+            self.includeDependencies();
+        }
+    });
 
-    if (self.includeAppDependencies === undefined) {
-        self.includeAppDependencies = true;
-    }
-
-    if (self.includeHtml === undefined) {
-        self.includeHtml = false;
-    }
-
-    if (!self.deferIncludes) {
-        self.includeDependencies();
-    }
-});
+    afterEach(function () {
+        self = null;
+    });
+})();
