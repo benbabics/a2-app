1bf6b7177d2733f1257cc999920f9419c5e0e5fe
James Baltos <james.baltos@wexinc.com>
2017-01-13 16:59:01 -0500

    Fix LoadingIndicator tests to use 'this'

--- test/unit/shared/widgets/loadingIndicator.service.spec.js
+++ test/unit/shared/widgets/loadingIndicator.service.spec.js
@@ -3,93 +3,94 @@
 
     describe("A LoadingIndicator service", function () {
 
-        var LoadingIndicator,
-            $ionicLoading,
-            $q,
-            $rootScope;
+        var self;
 
         beforeAll(function () {
             this.commonSharedMockExclusions = ["LoadingIndicator"];
         });
 
         beforeEach(function () {
+            self = this;
+
             //mock dependencies
-            $ionicLoading = jasmine.createSpyObj("$ionicLoading", ["hide", "show"]);
+            self.$ionicLoading = jasmine.createSpyObj("$ionicLoading", ["hide", "show"]);
 
             module(function ($provide) {
-                $provide.value("$ionicLoading", $ionicLoading);
+                $provide.value("$ionicLoading", self.$ionicLoading);
             });
 
             inject(function (_$q_, _$rootScope_, _LoadingIndicator_) {
-                LoadingIndicator = _LoadingIndicator_;
-                $q = _$q_;
-                $rootScope = _$rootScope_;
+                self.LoadingIndicator = _LoadingIndicator_;
+                self.$q = _$q_;
+                self.$rootScope = _$rootScope_;
             });
         });
 
-        describe("has an app:loadingBegin event handler function that", function () {
-            var showDeferred;
+        afterAll(function () {
+            self = null;
+        });
 
+        describe("has an app:loadingBegin event handler function that", function () {
             beforeEach(function() {
-                showDeferred = $q.defer();
-                $ionicLoading.show.and.returnValue(showDeferred.promise);
+                this.showDeferred = this.$q.defer();
+                this.$ionicLoading.show.and.returnValue(this.showDeferred.promise);
             });
 
             describe("when the loadingIndicatorCount > 0", function() {
                 beforeEach(function() {
-                    LoadingIndicator.begin(); // Increments loadingIndicatorCount
-                    $rootScope.$emit("app:loadingBegin");
+                    this.LoadingIndicator.begin(); // Increments loadingIndicatorCount
+                    this.$rootScope.$emit("app:loadingBegin");
                 });
 
                 it("should call $ionicLoading.show", function () {
-                    expect($ionicLoading.show).toHaveBeenCalled();
+                    expect(this.$ionicLoading.show).toHaveBeenCalled();
                 });
 
                 it("should display an ion-spinner", function () {
-                    expect($ionicLoading.show.calls.mostRecent().args).toEqual([{
+                    expect(this.$ionicLoading.show.calls.mostRecent().args).toEqual([{
                         template: "<ion-spinner class='spinner-light'></ion-spinner>"
                     }]);
                 });
 
                 describe("when the $ionicLoading.show promise is resolved", function() {
                     beforeEach(function() {
-                        spyOn($rootScope, "$emit");
-                        spyOn(LoadingIndicator, "complete");
-                        showDeferred.resolve();
-                        $rootScope.$digest();
+                        spyOn(this.$rootScope, "$emit");
+                        spyOn(this.LoadingIndicator, "complete");
+                        this.showDeferred.resolve();
+                        this.$rootScope.$digest();
                     });
 
                     it("should NOT call complete()", function() {
-                        expect($rootScope.$emit).not.toHaveBeenCalledWith("app:loadingComplete");
+                        expect(this.$rootScope.$emit).not.toHaveBeenCalledWith("app:loadingComplete");
                     });
                 });
             });
 
             describe("when the loadingIndicatorCount == 0", function() {
                 beforeEach(function() {
-                    $rootScope.$emit("app:loadingBegin");
+                    this.$rootScope.$emit("app:loadingBegin");
                 });
 
                 it("should call $ionicLoading.show", function () {
-                    expect($ionicLoading.show).toHaveBeenCalled();
+                    expect(this.$ionicLoading.show).toHaveBeenCalled();
                 });
 
                 it("should display an ion-spinner", function () {
-                    expect($ionicLoading.show.calls.mostRecent().args).toEqual([{
+                    expect(this.$ionicLoading.show.calls.mostRecent().args).toEqual([{
                         template: "<ion-spinner class='spinner-light'></ion-spinner>"
                     }]);
                 });
 
                 describe("when the $ionicLoading.show promise is resolved", function() {
                     beforeEach(function() {
-                        spyOn($rootScope, "$emit");
-                        spyOn(LoadingIndicator, "complete");
-                        showDeferred.resolve();
-                        $rootScope.$digest();
+                        spyOn(this.$rootScope, "$emit");
+                        spyOn(this.LoadingIndicator, "complete");
+                        this.showDeferred.resolve();
+                        this.$rootScope.$digest();
                     });
 
                     it("should call complete()", function() {
-                        expect($rootScope.$emit).toHaveBeenCalledWith("app:loadingComplete");
+                        expect(this.$rootScope.$emit).toHaveBeenCalledWith("app:loadingComplete");
                     });
                 });
             });
@@ -98,51 +99,52 @@
         describe("has an app:loadingComplete event handler function that", function () {
 
             beforeEach(function () {
-                $rootScope.$emit("app:loadingComplete");
+                this.$rootScope.$emit("app:loadingComplete");
             });
 
             it("should call $ionicLoading.hide", function () {
-                expect($ionicLoading.hide).toHaveBeenCalledWith();
+                expect(this.$ionicLoading.hide).toHaveBeenCalledWith();
             });
         });
 
         describe("has a begin function that", function () {
             beforeEach(function () {
-                spyOn($rootScope, "$emit");
+                spyOn(this.$rootScope, "$emit");
             });
 
             it("should broadcast the app:loadingBegin event when the first call is made.", function () {
-                LoadingIndicator.begin();
+                this.LoadingIndicator.begin();
 
-                expect($rootScope.$emit).toHaveBeenCalledWith("app:loadingBegin");
+                expect(this.$rootScope.$emit).toHaveBeenCalledWith("app:loadingBegin");
             });
 
             it("should not emit the app:begin event when subsequent calls are made", function () {
-                LoadingIndicator.begin();
-                LoadingIndicator.begin();
+                this.LoadingIndicator.begin();
+                this.LoadingIndicator.begin();
 
-                expect($rootScope.$emit.calls.count()).toBe(1);
+                expect(this.$rootScope.$emit.calls.count()).toBe(1);
             });
         });
 
         describe("has a complete function that", function () {
             beforeEach(function () {
-                spyOn($rootScope, "$emit");
+                spyOn(this.$rootScope, "$emit");
             });
 
             it("should not emit the app:loadingComplete event when multiple calls to begin were made", function () {
-                LoadingIndicator.begin();
-                LoadingIndicator.begin();
-                LoadingIndicator.complete();
+                this.LoadingIndicator.begin();
+                this.LoadingIndicator.begin();
+                this.LoadingIndicator.complete();
 
-                expect($rootScope.$emit).not.toHaveBeenCalledWith("app:loadingComplete");
+                expect(this.$rootScope.$emit).not.toHaveBeenCalledWith("app:loadingComplete");
             });
 
             it("should emit the app:loadingComplete event when the number of complete calls equals the number of begin calls", function () {
-                LoadingIndicator.begin();
-                LoadingIndicator.complete();
+                this.LoadingIndicator.begin();
+                this.LoadingIndicator.complete();
+                this.LoadingIndicator.complete();
 
-                expect($rootScope.$emit).toHaveBeenCalledWith("app:loadingComplete");
+                expect(this.$rootScope.$emit).toHaveBeenCalledWith("app:loadingComplete");
             });
         });
     });
