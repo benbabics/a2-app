0c43a5d21669d24b6c0e9a7e9229cd7e3bcfd549
James Baltos <james.baltos@wexinc.com>
2017-01-13 18:38:28 -0500

    Grab StatusBar element on function call instead of at activation

--- www/app/shared/widgets/statusBar.service.js
+++ www/app/shared/widgets/statusBar.service.js
@@ -2,28 +2,19 @@
     "use strict";
 
     // Service for controlling the iOS status bar overlaying the app
-    function StatusBar($document) {
-        //Private members
-        var element;
+    function StatusBar() {
 
         var service = {
             setOverlaysApp: setOverlaysApp
         };
 
-        activate();
-
         return service;
-
-        function activate() {
-            $document.ready(function() {
-                element = angular.element(document.querySelector("#status-bar-overlay"));
-                setOverlaysApp(true);
-            });
-        }
 
         // If TRUE is passed, the status bar will appear over the web view.
         // If FALSE is passed, the status bar will appear over a solid bar.
         function setOverlaysApp(overlays) {
+            var element = angular.element(document.querySelector("#status-bar-overlay"));
+
             if (overlays) {
                 element.addClass("overlaying");
             } else {
