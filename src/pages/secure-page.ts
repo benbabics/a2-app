8714fae133190fb6b91a068906d2c15943e231a5
mthompson <mychal.thompson@wexinc.com>
2017-04-02 22:23:10 -0400

    Added an entry guard to SecurePage

--- src/pages/secure-page.ts
+++ src/pages/secure-page.ts
@@ -4,6 +4,10 @@ import { Session } from "../models";
 
 export abstract class SecurePage extends Page {
 
+  ionViewCanEnter(): boolean {
+    return SessionManager.hasActiveSession; //TODO check if session is valid
+  }
+
   public get session(): Session {
     return SessionManager.currentSession;
   }
