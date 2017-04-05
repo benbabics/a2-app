c4a1ae3708d7f059ca8bcf95e4ad2e31cf087fb5
mthompson <mychal.thompson@wexinc.com>
2017-04-04 22:58:49 -0400

    Initial cards page implementation

--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -1,7 +1,9 @@
+import * as _ from "lodash";
 import { Component } from "@angular/core";
 import { NavParams } from "ionic-angular";
-import { WexNavController } from "../../providers";
+import { WexNavController, CardProvider } from "../../providers";
 import { SecurePage } from "../secure-page";
+import { Card } from "../../models";
 
 @Component({
   selector: "page-cards",
@@ -9,7 +11,41 @@ import { SecurePage } from "../secure-page";
 })
 export class CardsPage extends SecurePage {
 
-  constructor(public navCtrl: WexNavController, public navParams: NavParams) {
+  private static readonly SEARCH_FILTER_FIELDS: (keyof Card)[] = [
+    "embossingValue1",
+    "embossingValue2",
+    "cardId"
+  ];
+
+  public cards: Card[] = [];
+  public filteredCards: Card[] = [];
+  public fetchingCards: boolean = false;
+  public searchFilter: string = "";
+
+  constructor(public navCtrl: WexNavController, public navParams: NavParams, private cardProvider: CardProvider) {
     super("Cards");
+  }
+
+  ionViewWillEnter() {
+    this.fetchingCards = true;
+
+    this.cardProvider.search(this.session.user.company.accountId)
+      .finally(() => this.fetchingCards = false)
+      .subscribe((cards: Card[]) => {
+        this.filteredCards = this.cards = cards;
+
+        if (this.searchFilter) {
+          this.updateList();
+        }
+      });
+  }
+
+  public updateList() {
+    //do a case-insensitive search
+    let searchRegex = new RegExp(_.escapeRegExp(this.searchFilter), "i");
+
+    this.filteredCards = this.cards.filter((card) => CardsPage.SEARCH_FILTER_FIELDS.some((searchField) => {
+      return String(card[searchField]).search(searchRegex) !== -1;
+    }));
   }
 }
