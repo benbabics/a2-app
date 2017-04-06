a1848fc2d797803ce15a9812726f32ac01140c2b
mthompson <mychal.thompson@wexinc.com>
2017-04-05 22:37:53 -0400

    Added a sticky divder mode to WexList for handling grouped lists

--- src/components/wex-list/wex-list.ts
+++ src/components/wex-list/wex-list.ts
@@ -2,6 +2,11 @@ import * as _ from "lodash";
 import { Component, Input, TemplateRef, ContentChild, Query } from "@angular/core";
 import { Value } from "../../decorators/value";
 
+interface ItemListGroup {
+  heading: string;
+  items: any[];
+}
+
 @Component({
   selector: "wex-list",
   templateUrl: "wex-list.html"
@@ -11,12 +16,29 @@ export class WexList {
   @Value("COMPONENTS.LIST") CONSTANTS: any;
 
   @Input() items: any[];
+  @Input() dividers: string[];
   @Input() loading: boolean;
   @Input() headerFields: string[];
 
   @ContentChild(TemplateRef) itemTemplate: Query;
 
-  public get hasNoRecords(): boolean {
-    return !(this.loading || _.size(this.items) > 0);
+  public get hasNoItems(): boolean {
+    const hasNoItems = (itemList: any[]) => !(this.loading || _.size(itemList) > 0);
+
+    if (_.size(this.dividers) > 0) {
+      return !this.items || this.items.every(hasNoItems);
+    }
+    else {
+      return hasNoItems(this.items);
+    }
+  }
+
+  public getListForDivider(dividerIndex: number): any[] {
+    if(_.isNumber(dividerIndex) && dividerIndex < this.items.length) {
+      return this.items[dividerIndex];
+    }
+    else {
+      return null;
+    }
   }
 }
