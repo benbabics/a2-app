34b203eb8d8c775a5945c4142009bcb65b165d34
mthompson <mychal.thompson@wexinc.com>
2017-04-08 21:54:00 -0400

    Fixed issue with wex date pipe

--- src/pipes/wex-date.ts
+++ src/pipes/wex-date.ts
@@ -1,7 +1,6 @@
 import * as moment from "moment";
 import { Injectable, Pipe } from "@angular/core";
 import { Value } from "../decorators/value";
-import { DatePipe } from "@angular/common";
 
 @Pipe({
   name: "wexDate"
@@ -11,8 +10,6 @@ export class WexDate {
 
   @Value("LOCALE") private static readonly LOCALE: string;
   @Value("DATETIME") private static readonly CONSTANTS: any;
-
-  private datePipe: DatePipe = new DatePipe(WexDate.LOCALE);
 
   public transform(value: string, ignoreOffset: boolean) {
 
@@ -24,6 +21,6 @@ export class WexDate {
       return moment.parseZone(value).format(WexDate.CONSTANTS.DATE_FORMAT);
     }
 
-    return this.datePipe.transform(value, WexDate.CONSTANTS.DATE_FORMAT);
+    return moment(value).format(WexDate.CONSTANTS.DATE_FORMAT);
   }
 }
