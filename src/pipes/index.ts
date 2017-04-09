6a6aaabd48a88cc8983809848e2d477c69142662
mthompson <mychal.thompson@wexinc.com>
2017-04-09 19:22:38 -0400

    Added WexCardNumber pipe

--- src/pipes/index.ts
+++ src/pipes/index.ts
@@ -1,2 +1,3 @@
+export * from "./wex-card-number";
 export * from "./wex-currency";
 export * from "./wex-date";
---
+++ src/pipes/wex-card-number.ts
@@ -0,0 +1,15 @@
+import { Injectable, Pipe } from "@angular/core";
+
+@Pipe({
+  name: "wexCardNumber"
+})
+@Injectable()
+export class WexCardNumberPipe {
+
+  private static readonly CARD_MASK = "****";
+  private static readonly NUM_DIGITS = 5;
+
+  public transform(value: any) {
+    return `${WexCardNumberPipe.CARD_MASK}${String(value).slice(-WexCardNumberPipe.NUM_DIGITS)}`;
+  }
+}
