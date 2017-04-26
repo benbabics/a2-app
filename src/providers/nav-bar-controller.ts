c9a294ec3d0bd730c36b09a72c93fc693cbbe7a4
mthompson <mychal.thompson@wexinc.com>
2017-04-26 16:13:22 -0400

    Refactored NavBarController logic

--- src/providers/nav-bar-controller.ts
+++ src/providers/nav-bar-controller.ts
@@ -1,4 +1,4 @@
-import { App, Tabs, NavOptions } from "ionic-angular";
+import { App, Tabs, NavOptions, Tab } from "ionic-angular";
 import { Injectable } from "@angular/core";
 
 @Injectable()
@@ -17,10 +17,22 @@ export class NavBarController {
   constructor(private app: App) { }
 
   public get ionTabs(): Tabs {
-    return this.app.getRootNav() as any as Tabs;
+    return this.app.getRootNav().getActiveChildNav() as Tabs;
   }
 
-  public push(page: Function, params?: NavOptions) {
+  public getTab(page: Function): Tab {
+    let tabIndex = this.pages.indexOf(page.name);
+
+    if (tabIndex === -1) {
+      let error = `Failed to transition to page ${page.name} (not found)`;
+      console.error(error);
+      return null;
+    }
+
+    return this.ionTabs.getByIndex(tabIndex);
+  }
+
+  public select(page: Function, options?: NavOptions) {
     let tabIndex = this.pages.indexOf(page.name);
 
     if (tabIndex === -1) {
@@ -28,6 +40,7 @@ export class NavBarController {
       return;
     }
 
-    this.ionTabs.select(tabIndex, params);
+    this.ionTabs.select(tabIndex, options);
+    return Promise.resolve();
   }
 }
