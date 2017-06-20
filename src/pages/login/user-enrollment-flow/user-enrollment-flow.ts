3b1720e1792b1565a28e941df2db37695109440a
Ethan Standel <ethan.standel@wexinc.com>
2017-06-19 22:40:03 -0400

    Added all responses and starting browser

--- src/pages/login/user-enrollment-flow/user-enrollment-flow.ts
+++ src/pages/login/user-enrollment-flow/user-enrollment-flow.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { InAppBrowser } from "@ionic-native/in-app-browser";
+import { InAppBrowser, InAppBrowserObject } from "@ionic-native/in-app-browser";
 import { Http } from "@angular/http";
 import { Page } from "../../page"
 import { GetCurrentEnvironmentConstants, ConstantsInfo } from "../../../app/app.constants";
@@ -13,6 +13,8 @@ import { AlertController } from "ionic-angular";
 export class UserEnrollmentFlow extends Page {
   @Input() text: string;
 
+  private isLoading: boolean = false;
+
   private ENROLLMENT_URL = (GetCurrentEnvironmentConstants().APIS as any).ONLINE_ENROLLMENT.BASE_URL;
   private ENROLLMENT_PING = this.ENROLLMENT_URL + "ping";
 
@@ -25,30 +27,37 @@ export class UserEnrollmentFlow extends Page {
    }
 
   private handleOpenEnrollmentWindow(): void {
+    this.isLoading = true;
     this.http.get( this.ENROLLMENT_PING )
         .subscribe(
             success => this.loadOnlineEnrollmentApp(),
-            failure => this.displayServiceUnavailableAlert()
+            failure => this.displayServiceUnavailableAlert(null)
         )
   }
 
   private loadOnlineEnrollmentApp() {
+    this.isLoading = false;
     let browser = this.inAppBrowser.create(this.ENROLLMENT_URL);
-    browser.on("loadstop").subscribe(this.closeOnlineEnrollmentAppWithErrorAlert);
-    browser.on("loaderror").subscribe(this.closeOnlineEnrollmentAppWithErrorAlert);
+    browser.on("loaderror").subscribe(() => this.closeOnlineEnrollmentAppWithErrorAlert(browser));
   }
 
-  private displayServiceUnavailableAlert() {
+  private displayServiceUnavailableAlert(browser: InAppBrowserObject) {
+    if (browser) {
+      browser.close();
+    }
     this.alertController.create({
         subTitle: this.CONSTANTS.MESSAGES.ERRORS.serviceUnavailable,
         buttons: [ConstantsInfo.Common.BUTTONS.OK]
     }).present();
   }
 
-  private closeOnlineEnrollmentAppWithErrorAlert() {
-          this.alertController.create({
-        subTitle: this.CONSTANTS.MESSAGES.ERRORS.applicationError,
-        buttons: [ConstantsInfo.Common.BUTTONS.OK]
+  private closeOnlineEnrollmentAppWithErrorAlert(browser: InAppBrowserObject) {
+    if (browser) {
+      browser.close();
+    }
+    this.alertController.create({
+      subTitle: this.CONSTANTS.MESSAGES.ERRORS.applicationError,
+      buttons: [ConstantsInfo.Common.BUTTONS.OK]
     }).present();
   }
 }
