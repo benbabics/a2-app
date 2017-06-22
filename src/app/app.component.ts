2985042cb9e001f9f15fa468a4433151ec931952
mthompson <mychal.thompson@wexinc.com>
2017-06-22 16:36:27 -0400

    iOS style changes (WIP)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+import { Keyboard } from "@ionic-native/keyboard";
 
 import { LoginPage } from "../pages/login/login";
 import { SessionManager } from "./../providers/session-manager";
@@ -14,14 +15,26 @@ import "chart.js";
 })
 export class MyApp {
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private wexAppBannerController: WexAppBannerController) {
+  constructor(keyboard: Keyboard, platform: Platform, private statusBar: StatusBar, splashScreen: SplashScreen, private wexAppBannerController: WexAppBannerController, sessionManager: SessionManager) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
 
       statusBar.styleDefault();
       splashScreen.hide();
+      keyboard.disableScroll(true);
     });
+
+    sessionManager.sessionStateObserver.subscribe(session => this.onSessionChange(session));
+  }
+
+  private onSessionChange(session) {
+    if (session) {
+      this.statusBar.overlaysWebView(false);
+    }
+    else {
+      this.statusBar.overlaysWebView(true);
+    }
   }
 
   public get hasBannerContent(): boolean {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import {
 
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+import { Keyboard } from "@ionic-native/keyboard";
 import {
   AuthProvider,
   CardProvider,
@@ -136,6 +137,7 @@ import { ModelGeneratorsModule } from "@angular-wex/models/mocks";
     StatusBar,
     SplashScreen,
     AppVersion,
+    Keyboard,
     //# app providers
     //----------------------
     {
