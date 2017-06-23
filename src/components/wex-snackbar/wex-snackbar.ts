b8f96e9101e840510da098bd25a68e0f92dd5801
Ethan Standel <ethan.standel@wexinc.com>
2017-06-23 15:20:49 -0400

    Replaced wex-banner

--- src/components/wex-snackbar/wex-snackbar.ts
+++ src/components/wex-snackbar/wex-snackbar.ts
@@ -11,28 +11,30 @@ import {
 
 type milliseconds = number;
 
-interface WexSnackbarAction {
+export interface WexSnackbarAction {
   label: string;
   handler: () => boolean | void;
 }
 
 const ANIMATION_DELAY: milliseconds = 200;
 
+export const WEX_SNACKBAR_ANIMATIONS = [
+  trigger("fadeInOut", [
+    transition(":enter", [
+      style({ opacity: 0 }),
+      animate(ANIMATION_DELAY, style({ opacity: 1.0 }))
+    ]),
+    transition(":leave", [
+      style({ opacity: 1.0 }),
+      animate(ANIMATION_DELAY, style({ opacity: 0 }))
+    ]),
+  ])
+];
+
 @Component({
   selector: "wex-snackbar",
   templateUrl: "wex-snackbar.html",
-  animations: [
-    trigger("fadeInOut", [
-      transition(":enter", [
-        style({ opacity: 0 }),
-        animate(ANIMATION_DELAY, style({ opacity: 1.0 }))
-      ]),
-      transition(":leave", [
-        style({ opacity: 1.0 }),
-        animate(ANIMATION_DELAY, style({ opacity: 0 }))
-      ]),
-    ])
-  ]
+  animations: WEX_SNACKBAR_ANIMATIONS
 })
 export class WexSnackbar {
 
@@ -49,17 +51,20 @@ export class WexSnackbar {
   ngAfterViewInit() {
     if (Number(this.dismissAfter) > 0) {
       setTimeout(() => {
-        this.dismissed = true;
-        // wait for the animation to finish before emitting
-        setTimeout(() => this.dismissedEmitter.emit(), ANIMATION_DELAY*2);
+
       }, this.dismissAfter);
     }
   }
 
+  private dismiss() {
+    this.dismissed = true;
+    // wait for the animation to finish before emitting
+    setTimeout(() => this.dismissedEmitter.emit(), ANIMATION_DELAY * 2);
+  }
+
   private performAction() {
     if (this.action.handler()) {
-      this.dismissed = true;
-      setTimeout(() => this.dismissedEmitter.emit(), ANIMATION_DELAY*2);
+      this.dismiss();
     }
   }
 
