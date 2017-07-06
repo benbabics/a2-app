2080066885752ead8f2c68e16c901241e16c630a
mthompson <mychal.thompson@wexinc.com>
2017-07-06 17:45:39 -0400

    Added WexKeyboardAware directive, WexInvoiceDisplay component. More WIP Add Payment UI work

--- src/pages/payments/add/add-payment.ts
+++ src/pages/payments/add/add-payment.ts
@@ -1,4 +1,5 @@
 import * as _ from "lodash";
+import * as accounting from "accounting-js";
 import { Component } from "@angular/core";
 import { NavParams, NavController, ViewController } from "ionic-angular";
 import { SecurePage } from "../../secure-page";
@@ -6,12 +7,16 @@ import { SessionManager } from "../../../providers";
 import { PaymentProvider } from "@angular-wex/api-providers";
 import { Session } from "../../../models";
 import { InvoiceSummary } from "@angular-wex/models";
+import { WexValidateCurrencyParams } from "@angular-wex/validators";
 
 @Component({
   selector: "page-add-payment",
   templateUrl: "add-payment.html"
 })
 export class AddPaymentPage extends SecurePage {
+
+  private _paymentAmount: number;
+  private focused: boolean = false;
 
   constructor(
     sessionManager: SessionManager,
@@ -27,6 +32,30 @@ export class AddPaymentPage extends SecurePage {
 
   public get invoiceSummary(): InvoiceSummary {
     return this.session.invoiceSummary;
+  }
+
+  public get paymentAmount(): string {
+    if (_.isNumber(this._paymentAmount)) {
+      if (this.focused) {
+        return accounting.unformat(this._paymentAmount);
+      }
+      else {
+        return accounting.format(this._paymentAmount);
+      }
+    }
+
+    return "";
+  }
+
+  public set paymentAmount(paymentAmount: string) {
+    this._paymentAmount = accounting.unformat(paymentAmount);
+  }
+
+  public get validateCurrencyOptions(): WexValidateCurrencyParams {
+    return {
+      minimumAmount: 0,
+      maximumAmount: this.invoiceSummary.details.currentBalance
+    };
   }
 
   public cancel(data?: any) {
