1be1d91a973523c39894835692b6ae26e95a9d83
mthompson <mychal.thompson@wexinc.com>
2017-07-06 17:45:39 -0400

    Added WexKeyboardAware directive, WexInvoiceDisplay component. More WIP Add Payment UI work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,8 @@ import {
   WexAppBanner,
   WexSnackbar,
   WexStaticListPageHeader,
-  WexStaticListPageContent
+  WexStaticListPageContent,
+  WexInvoiceDisplay
 } from "../components";
 
 import { StatusBar } from "@ionic-native/status-bar";
@@ -64,10 +65,12 @@ import { ContactUsPage } from "../pages/contact-us/contact-us";
 import {
   WexIfPlatformDirective,
   WexIfPlatformAndroidDirective,
-  WexIfPlatformIosDirective
+  WexIfPlatformIosDirective,
+  WexKeyboardAware
 } from "../directives";
 import { InAppBrowser } from "@ionic-native/in-app-browser";
 import { AddPaymentPage } from "../pages/payments/add/add-payment";
+import { AngularWexValidatorsModule } from "@angular-wex/validators";
 
 @NgModule({
   declarations: [
@@ -110,7 +113,9 @@ import { AddPaymentPage } from "../pages/payments/add/add-payment";
     WexIfPlatformDirective,
     WexIfPlatformAndroidDirective,
     WexIfPlatformIosDirective,
-    ContactUsPage
+    ContactUsPage,
+    WexInvoiceDisplay,
+    WexKeyboardAware
   ],
   imports: [
     //# Angular
@@ -123,6 +128,7 @@ import { AddPaymentPage } from "../pages/payments/add/add-payment";
     //# WEX
     //----------------------
     ApiProviders.withConstants(GetCurrentEnvironmentConstants),
+    AngularWexValidatorsModule,
     //# third party dependencies
     //----------------------
     ChartsModule,
