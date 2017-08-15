7c8c9a58f918c73914a5b227f38d04efb20260ab
mthompson <mychal.thompson@wexinc.com>
2017-08-15 17:16:07 -0400

    Added initial transaction grouping by date

--- src/pages/transactions/transactions.ts
+++ src/pages/transactions/transactions.ts
@@ -1,8 +1,9 @@
 import { Observable } from 'rxjs';
 import * as _ from "lodash";
+import * as moment from "moment";
 import { Component, Injector } from "@angular/core";
 import { NavController, NavParams, SegmentButton } from "ionic-angular";
-import { StaticListPage } from "../static-list-page";
+import { StaticListPage, GroupedList } from "../static-list-page";
 import { Session, TransactionList, DynamicList } from "../../models";
 import { WexGreeking } from "../../components";
 import { SessionCache, PostedTransactionRequestor, DynamicSessionListInfoRequestor } from "../../providers";
@@ -204,7 +205,7 @@ class TransactionsPageFilteredListView extends TransactionsPageDateView {
 
     if (doRequest) {
       return this.requestor.request(this.transactionsPage.session, {
-        filterType: this.filterType,
+        filterBy: this.filterType,
         filterValue: this.filterValue
       }).map(response => response.items);
     }
@@ -244,9 +245,52 @@ export class TransactionsPage extends StaticListPage<TransactionListModelType, T
   constructor(public navCtrl: NavController, public navParams: NavParams, public injector: Injector) {
     super("Transactions", injector);
 
+    this.listGroupDisplayOrder = [];
     this.filter = this.navParams.get(TransactionsParams.Filter);
     this.selectList(navParams.get(TransactionsParams.SelectedList) || TransactionListType.CardNumber);
   }
+
+  private calculateLabelGroupByDate(date: Date): string {
+    let offsetDays: number = moment().diff(date, "days");
+    let $moment: moment.Moment = moment(date).subtract(offsetDays);
+
+    if (offsetDays === 0) {
+      return "Today";
+    }
+    else if (offsetDays === 1) {
+      return "Yesterday";
+    }
+    else if (offsetDays < 30) {
+      return $moment.format("MM/DD/YYYY");
+    }
+    else if (offsetDays < 360) {
+      return $moment.format("MMMM YYYY");
+    }
+    else {
+      return $moment.format("YYYY");
+    }
+  }
+
+  private calculateDateByLabelGroup(labelGroup: string): Date {
+    if (labelGroup === "Today") {
+      return new Date();
+    }
+    else if (labelGroup === "Yesterday") {
+      return moment().subtract(1, "days").toDate();
+    }
+    else if (/^\d{2}\/\d{2}\/\d{4,}$/.test(labelGroup)) {
+      return moment(labelGroup, "MM/DD/YYYY").toDate();
+    }
+    else if (/^\D+ \d{4,}$/.test(labelGroup)) {
+      return moment(labelGroup, "MMMM YYYY").toDate();
+    }
+    else if (/^\d{4,}$/.test(labelGroup)) {
+      return moment(labelGroup, "YYYY").toDate();
+    }
+
+    return null;
+  }
+
 
   private selectList(listType: TransactionListType) {
     if (this.selectedListView && this.selectedListView.type === listType) {
@@ -272,8 +316,33 @@ export class TransactionsPage extends StaticListPage<TransactionListModelType, T
     return this.selectedListView.fetch(options);
   }
 
+  protected groupItems(transactions: Transaction[]): GroupedList<Transaction> {
+    // Group the transactions by date
+    let groupedList = transactions.reduce<GroupedList<Transaction>>((groupedList, transaction) => {
+      // Get the correct group for this transaction
+      let group = this.calculateLabelGroupByDate(transaction.postDate);
+      // Get the list for this group
+      let transactionList = groupedList[group] = groupedList[group] || [];
+      // Add the transaction to the list
+      transactionList.push(transaction);
+
+      return groupedList;
+    }, {});
+
+    // Calculate the list group display order
+    this.listGroupDisplayOrder = _.keys(groupedList).sort((groupA, groupB) => {
+      return moment(this.calculateDateByLabelGroup(groupA)).isAfter(this.calculateDateByLabelGroup(groupB)) ? -1 : 1;
+    });
+
+    return groupedList;
+  }
+
   protected sortItems(items: TransactionListModelType[]): TransactionListModelType[] {
     return this.selectedListView.sortItems(items);
+  }
+
+  public get dividerLabels(): string[] {
+    return this.isGrouped ? this.listGroupDisplayOrder : undefined;
   }
 
   public get filterBy(): PostedTransactionSearchFilterBy {
@@ -304,6 +373,10 @@ export class TransactionsPage extends StaticListPage<TransactionListModelType, T
 
   public get isDriverView(): boolean {
     return this.selectedListView.type === TransactionListType.DriverName;
+  }
+
+  public get isGrouped(): boolean {
+    return this.isDateView;
   }
 
   public get isListSelectionLocked(): boolean {
--- src/providers/session-info-requestor/default-session-info-requestors.ts
+++ src/providers/session-info-requestor/default-session-info-requestors.ts
@@ -64,7 +64,7 @@ export class DefaultSessionInfoRequestors extends SessionInfoRequestors {
     requestor: (session: Session) => this.driverProvider.search(session.user.company.details.accountId)
   };
 
-  private readonly postedTransactionsInfoRequestor: SessionInfoRequestorDetails = new PostedTransactionRequestor(this.transactionProvider);
+  private readonly postedTransactionsInfoRequestor: SessionInfoRequestorDetails = new SessionPostedTransactionRequestor(this.transactionProvider);
 
   private readonly postedTransactionsRequestor: SessionInfoRequestorDetails = {
     requiredFields: [Session.Field.PostedTransactionsInfo],
@@ -125,11 +125,26 @@ export class PostedTransactionRequestor extends DynamicSessionListInfoRequestor<
 
   protected readonly listMergeId: keyof Transaction.Details = "transactionId";
 
-  constructor(private transactionProvider: TransactionProvider, public dynamicList: TransactionList = SessionCache.cachedValues.postedTransactionsInfo) {
+  constructor(private transactionProvider: TransactionProvider, public dynamicList: TransactionList) {
     super(Transaction, [Session.Field.User]);
   }
 
   protected search(session: Session, params: any): Observable<ListResponse<Transaction>> {
      return this.transactionProvider.searchPosted(session.user.billingCompany.details.accountId, params);
+  }
+}
+
+export class SessionPostedTransactionRequestor extends PostedTransactionRequestor {
+
+  constructor(transactionProvider: TransactionProvider) {
+    super(transactionProvider, undefined);
+  }
+
+  public get dynamicList(): TransactionList {
+    return SessionCache.cachedValues.postedTransactionsInfo;
+  }
+
+  public set dynamicList(dynamicList: TransactionList) {
+    SessionCache.cachedValues.postedTransactionsInfo = dynamicList;
   }
 }
