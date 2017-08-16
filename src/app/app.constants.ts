923f37c5625d79e5e62d371ef87c37308d870a7f
mthompson <mychal.thompson@wexinc.com>
2017-08-16 11:37:38 -0400

    Externalized labels

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -666,6 +666,10 @@ export namespace ConstantsInfo {
           ],
           greekedElementCount: 15
         },
+        LABELS: {
+          today: "Today",
+          yesterday: "Yesterday"
+        },
         title: "Transactions",
 
         //# Transaction Details page
