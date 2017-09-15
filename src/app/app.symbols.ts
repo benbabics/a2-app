7a33847f264b707f37acd5c1a52ed2eace5416ad
mthompson <mychal.thompson@wexinc.com>
2017-09-14 20:48:44 -0400

    Added snackbar message for when fingerprint authentication cannot be setup

--- src/app/app.symbols.ts
+++ src/app/app.symbols.ts
@@ -3,4 +3,5 @@ import { InjectionToken } from "@angular/core";
 export namespace AppSymbols {
 
   export const RootPage: InjectionToken<any> = new InjectionToken<any>("RootPage");
+  export const FingerprintAuthenticationTermsPage: InjectionToken<any> = new InjectionToken<any>("FingerprintAuthenticationTermsPage");
 }
--- src/providers/ui-notifications-controller.ts
+++ src/providers/ui-notifications-controller.ts
@@ -1,26 +1,31 @@
 import * as _ from "lodash";
-import { Injectable } from "@angular/core";
+import { Injectable, Inject } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { Subscriber } from "rxjs/Subscriber";
 import { SessionCache } from "./session-cache";
 import { Value } from "../decorators/value";
-import { FingerprintAuthenticationTermsPage } from "../pages/login/fingerprint-auth-terms/fingerprint-auth-terms";
 import { ModalController } from "ionic-angular";
 import { WexPlatform } from "./platform";
 import { WexAppSnackbarController } from "../components/wex-app-snackbar-controller/wex-app-snackbar-controller";
 import { NameUtils } from "../utils/name-utils";
 import { User } from "@angular-wex/models";
 import { Session } from "../models/session";
+import { AppSymbols } from "../app/app.symbols";
 
 @Injectable()
 export class UiNotificationsController {
 
   @Value("GLOBAL_NOTIFICATIONS.fingerprintSuccess.message")
   private readonly FINGERPRINT_SUCCESS: string;
+  @Value("GLOBAL_NOTIFICATIONS.fingerprintError.message")
+  private readonly FINGERPRINT_ERROR: string;
   @Value("GLOBAL_NOTIFICATIONS.fingerprintSuccess.duration")
   private readonly FINGERPRINT_SUCCESS_DURATION: number;
+  @Value("GLOBAL_NOTIFICATIONS.fingerprintError.duration")
+  private readonly FINGERPRINT_ERROR_DURATION: number;
 
   constructor(
+    @Inject(AppSymbols.FingerprintAuthenticationTermsPage) private fingerprintAuthenticationTermsPage: any,
     private sessionCache: SessionCache,
     private modalController: ModalController,
     private platform: WexPlatform,
@@ -40,8 +45,18 @@ export class UiNotificationsController {
     });
   }
 
+  public presentFingerprintProfileErrorMessage() {
+    this.wexAppSnackbarController.createQueued({
+      message: _.template(this.FINGERPRINT_ERROR)({
+        fingerprintTitle: this.platform.fingerprintTitle(true)
+      }),
+      duration: this.FINGERPRINT_ERROR_DURATION,
+      important: true
+    }).present();
+  }
+
   public promptFingerprintTerms(): Observable<boolean> {
-    let modal = this.modalController.create(FingerprintAuthenticationTermsPage);
+    let modal = this.modalController.create(this.fingerprintAuthenticationTermsPage, { enableBackdropDismiss: false });
     modal.present();
 
     return new Observable<boolean>((observer: Subscriber<boolean>) => {
