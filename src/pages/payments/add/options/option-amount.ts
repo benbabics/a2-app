e5f282496ca5f44718d2483a0ffe65b30edcc980
ethanstandel <ethan.standel@wexinc.com>
2017-10-19 14:09:38 -0400

    Forces list-styles to conform throughout payment flow and fixes currency input.

--- src/pages/payments/add/options/option-amount.ts
+++ src/pages/payments/add/options/option-amount.ts
@@ -1,10 +1,6 @@
-import * as _ from "lodash";
-import * as accounting from "accounting-js";
-import { TextInput, RadioButton } from 'ionic-angular';
+import { RadioButton } from "ionic-angular";
 import { Component, Input, ViewChild, OnInit } from "@angular/core";
-import { PaymentAmount, PaymentAmountTypes } from './../../../../providers/payment-service';
-
-type FormInputTypes = TextInput;
+import { PaymentAmount, PaymentAmountTypes } from "./../../../../providers/payment-service";
 
 @Component({
   selector: "option-amount",
@@ -13,14 +9,15 @@ type FormInputTypes = TextInput;
 export class OptionAmount implements OnInit {
 
   @Input() option: PaymentAmount;
-  @ViewChild("otherAmountInput") otherAmountInput: FormInputTypes;
   @ViewChild("radio") radio: RadioButton;
 
-  private _otherAmount: number;
+  public otherAmount: number;
   public keyOtherAmount = PaymentAmountTypes.OtherAmount;
 
   ngOnInit() {
-    this._otherAmount = this.option.value;
+    if (this.isOtherAmountOption) {
+      this.otherAmount = this.option.value;
+    }
   }
 
   public get isSelected(): boolean {
@@ -29,20 +26,5 @@ export class OptionAmount implements OnInit {
 
   public get isOtherAmountOption(): boolean {
     return this.option.key === this.keyOtherAmount;
-  }
-
-  public get otherAmount(): string {
-    if (_.isNumber(this._otherAmount)) {
-      if (this.otherAmountInput && this.otherAmountInput._isFocus ) {
-        return accounting.unformat(this._otherAmount);
-      }
-      else return accounting.format(this._otherAmount);
-    }
-
-    return "";
-  }
-
-  public set otherAmount(amount: string) {
-    this._otherAmount = accounting.unformat(amount);
   }
 }
