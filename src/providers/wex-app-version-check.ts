bd5e9ecd14e421f6d6942c9d37872df3d4dec1b6
ethanstandel <ethan.standel@wexinc.com>
2017-10-21 17:14:49 -0400

    Fixes version check from mock support.

--- src/providers/wex-app-version-check.ts
+++ src/providers/wex-app-version-check.ts
@@ -46,11 +46,10 @@ export class WexAppVersionCheck {
   }
 
   private checkVersionStatus(): Observable<VersionStatus> {
-    Observable.fromPromise(this.wexPlatform.ready(() => this.appVersion.getVersionNumber()))
+    return Observable.fromPromise(this.wexPlatform.ready(() => this.appVersion.getVersionNumber()))
       .map((versionNumber: string) => ConstantsInfo.Common.VERSION_NUMBER = versionNumber)
       .flatMap((versionNumber: string) => this.getStatus(versionNumber, this.clientId, this.platformName))
       .catch(() => Observable.of(VersionStatus.Supported));
-    return Observable.of(VersionStatus.Supported);
   }
 
   private getStatus(versionNumber: string, clientId: string, platform: string): Observable<VersionStatus> {
