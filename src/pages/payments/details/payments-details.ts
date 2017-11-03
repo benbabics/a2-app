3fe682eb2a9a11b0da25d9e2e1c513434462299e
mthompson <mychal.thompson@wexinc.com>
2017-11-03 11:40:13 -0400

    Cancel payments fix

--- src/pages/payments/details/payments-details.ts
+++ src/pages/payments/details/payments-details.ts
@@ -1,8 +1,9 @@
+import * as _ from "lodash";
 import { NavParams, NavController, App } from "ionic-angular";
 import { Component, Injector } from "@angular/core";
 import { DetailsPage } from "../../details-page";
 import { Payment } from "@angular-wex/models";
-import { WexPlatform } from "../../../providers";
+import { WexPlatform, SessionCache } from "../../../providers";
 import { AddPaymentPage } from "../add/add-payment";
 import { PaymentProvider } from "@angular-wex/api-providers";
 import { WexAlertController } from "../../../components/wex-alert-controller/wex-alert-controller";
@@ -24,6 +25,7 @@ export class PaymentsDetailsPage extends DetailsPage {
   public readonly DATE_FORMAT: string = "MMM D, YYYY";
 
   public payment: Payment;
+  public isCanceling: boolean;
 
   constructor(
     public navParams: NavParams,
@@ -52,7 +54,11 @@ export class PaymentsDetailsPage extends DetailsPage {
 
   public cancelPayment() {
     this.wexAlertController.confirmation(this.CONSTANTS.cancelPaymentConfirmation, () => {
+      this.isCanceling = true;
+
       this.paymentProvider.cancelPayment(this.session.user.billingCompany.details.accountId, this.payment.details.id)
+        .map(() => _.remove(SessionCache.cachedValues.payments, payment => payment.details.id === this.payment.details.id))
+        .map(() => this.isCanceling = false)
         .subscribe(() => this.navCtrl.pop());
     });
   }
