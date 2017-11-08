f01db834dbcd6a5ba125aa0ecbf5eb7150d711a8
ethanstandel <ethan.standel@wexinc.com>
2017-11-08 10:14:24 -0500

    Helps with keyboard pushing page out of frame issue.

--- src/directives/autofocus/autofocus.ts
+++ src/directives/autofocus/autofocus.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, DoCheck, HostListener } from "@angular/core";
+import { Keyboard } from "@ionic-native/keyboard";
 
 @Directive({
   selector: "[Autofocus]"
@@ -7,10 +8,11 @@ export class AutofocusDirective implements DoCheck {
   @Input("Autofocus") Autofocus: boolean;
   private hasFocused: boolean = false;
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, private keyboard: Keyboard) { }
 
   public ngDoCheck() {
     if (this.Autofocus === true && !this.hasFocused) {
+      this.keyboard.disableScroll(true);
       this.el.nativeElement.focus();
     } else if (this.Autofocus === false) {
       this.hasFocused = false;
@@ -20,5 +22,10 @@ export class AutofocusDirective implements DoCheck {
   @HostListener("focus")
   public onFocus() {
     this.hasFocused = true;
+  }
+
+  @HostListener("blur")
+  public onBlur() {
+    this.keyboard.disableScroll(false);
   }
 }
