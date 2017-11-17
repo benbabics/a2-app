a4b6c24cd7bd95554985c76975a8a4bdbd1c8a6c
Ben Babics <Ben.Babics@wexinc.com>
2017-11-17 15:23:37 -0500

    MOBACCTMGT-1389 android gray disabled button

--- src/components/action-indicator/action-indicator.ts
+++ src/components/action-indicator/action-indicator.ts
@@ -1,10 +1,28 @@
 import { Component, Input } from "@angular/core";
+import { ElementRef } from "@angular/core";
+import { OnChanges, SimpleChanges } from "@angular/core/src/metadata/lifecycle_hooks";
 
 @Component({
   selector: "action-indicator",
   templateUrl: "action-indicator.html"
 })
-export class ActionIndicator {
+export class ActionIndicator implements OnChanges {
 
   @Input("when") public showIndicator: boolean;
+
+  constructor(private elementRef: ElementRef) {}
+
+  private element(): any {
+    return this.elementRef.nativeElement;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    let hasIndicator = changes.showIndicator.currentValue;
+    let button = this.element().closest(".button-gradient");
+
+    if (button) {
+      button.classList.toggle("is-loading", hasIndicator);
+    }
+  }
+
 }
